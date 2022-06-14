c1008dc286b7c01d3b4db7c5e01d9de9e85045d3
Gonzalo Diaz <gdiazc@ripley.com>
2022-06-14 19:45:22 -0400

    Problem 0001: solved.

---
+++ src/problem0001_test.go
@@ -0,0 +1,28 @@
+/**
+ * Multiples of 3 and 5
+ *
+ * https://projecteuler.net/problem=1
+ *
+ * If we list all the natural numbers below 10 that are multiples
+ * of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+ *
+ * Find the sum of all the multiples of 3 or 5 below 1000.
+ */
+
+package projecteuler
+
+import (
+	"testing"
+)
+
+func TestProblem0001(t *testing.T) {
+
+	solutionFound := 233168
+	top := 1000
+
+	ans := Problem0001(top)
+
+	if ans != solutionFound {
+		t.Errorf("IntMin(2, -2) = %d; want -2", ans)
+	}
+}
