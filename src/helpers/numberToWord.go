85605e3be5f0bc8a978e45f39759d6f27d3c0b94
Gonzalo Diaz <devel@gon.cl>
2022-07-12 11:19:33 -0400

    Helper: number to word function. From 0, up to 1000. Better boundary testing.

--- src/helpers/numberToWord.go
+++ src/helpers/numberToWord.go
@@ -11,6 +11,7 @@ const _CENTS_ = "hundred"
 const _MILLS_ = "thousand"
 
 var dictionary = map[string]string{
+	"0":    "zero",
 	"1":    "one",
 	"2":    "two",
 	"3":    "three",
@@ -42,45 +43,71 @@ var dictionary = map[string]string{
 	"1000": "thousand",
 }
 
-func NumberToWord(_value big.Int) string {
+func NumberToWord(_value big.Int) (string, bool) {
 	number := _value.Text(__NUMERIC_BASE__)
 	digits := len(number)
 
 	var bottomLimit, upperLimit bool
 
 	/// 1 to 19
-	bottomLimit = _value.Cmp(big.NewInt(0)) == 1
+	bottomLimit = _value.Cmp(big.NewInt(0)) >= 0
 	upperLimit = _value.Cmp(big.NewInt(19)) <= 0
 
 	if digits <= 2 && bottomLimit && upperLimit {
-		return dictionary[number]
+		return dictionary[number], false
 	}
 
 	/// 20 to 99
-	bottomLimit = _value.Cmp(big.NewInt(20)) == 1
+	bottomLimit = _value.Cmp(big.NewInt(20)) >= 0
 	upperLimit = _value.Cmp(big.NewInt(99)) <= 0
 
 	if digits <= 2 && bottomLimit && upperLimit {
 
 		dec := new(big.Int)
-		div := big.NewInt(10)
 		unit := new(big.Int)
+		div := big.NewInt(10)
 
 		dec, unit = dec.DivMod(&_value, div, unit)
 		dec = dec.Mul(dec, big.NewInt((10)))
 
-		log.Info("dec => %s", dec.Text(__NUMERIC_BASE__))
-		log.Info("div => %s", div.Text(__NUMERIC_BASE__))
-		log.Info("unit => %s", unit.Text(__NUMERIC_BASE__))
+		log.Debug("dec => %s | div => %s | unit => %s",
+			dec.Text(__NUMERIC_BASE__),
+			div.Text(__NUMERIC_BASE__),
+			unit.Text(__NUMERIC_BASE__),
+		)
 
 		unitIsZero := unit.Cmp(big.NewInt(0))
 		if unitIsZero == 0 {
-			return dictionary[number]
+			return dictionary[number], false
 		}
 
-		return fmt.Sprintf("%s-%s", dictionary[dec.Text(__NUMERIC_BASE__)], dictionary[unit.Text(__NUMERIC_BASE__)])
-
+		return fmt.Sprintf("%s-%s",
+			dictionary[dec.Text(__NUMERIC_BASE__)],
+			dictionary[unit.Text(__NUMERIC_BASE__)],
+		), false
 	}
 
-	return ""
+	/// 100 to 999
+	if digits <= 3 {
+		cent, rest := new(big.Int).DivMod(&_value, big.NewInt(100), new(big.Int))
+
+		if rest.Cmp(big.NewInt(0)) == 0 {
+			return fmt.Sprintf("%s %s", dictionary[cent.Text(__NUMERIC_BASE__)], _CENTS_), false
+		}
+
+		last, err := NumberToWord(*rest)
+		log.Debug("err => %t", err) // in bounds, never happens
+
+		return fmt.Sprintf("%s %s and %s",
+			dictionary[cent.Text(__NUMERIC_BASE__)],
+			_CENTS_,
+			last), false
+	}
+
+	if _value.Cmp(big.NewInt(1000)) == 0 {
+		mills := new(big.Int).Div(&_value, big.NewInt(1000))
+		return fmt.Sprintf("%s %s", dictionary[mills.Text(__NUMERIC_BASE__)], _MILLS_), false
+	}
+
+	return "", false
 }
