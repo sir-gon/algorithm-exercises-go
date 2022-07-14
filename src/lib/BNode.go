bf71656b869bd9b32815a3b57fb435b16628d34b
Gonzalo Diaz <devel@gon.cl>
2022-07-14 15:47:53 -0400

    Library: BNode "class" (struct type). Chainable setters. New "isLeaft" method.

--- src/lib/BNode.go
+++ src/lib/BNode.go
@@ -5,26 +5,33 @@ type BNode struct {
 	left, right *BNode
 }
 
-func (node *BNode) setValue(_value int) {
+func (node *BNode) setValue(_value int) BNode {
 	node.value = _value
+	return *node
 }
 
 func (node BNode) getValue() int {
 	return node.value
 }
 
-func (node *BNode) setLeft(_left BNode) {
+func (node *BNode) setLeft(_left BNode) BNode {
 	node.left = &_left
+	return *node
 }
 
 func (node BNode) getLeft() *BNode {
 	return node.left
 }
 
-func (node *BNode) setRight(_right BNode) {
+func (node *BNode) setRight(_right BNode) BNode {
 	node.right = &_right
+	return *node
 }
 
 func (node BNode) getRight() *BNode {
 	return node.right
+}
+
+func (node BNode) isLeaf() bool {
+	return node.left == nil && node.right == nil
 }
--- src/lib/BNode_test.go
+++ src/lib/BNode_test.go
@@ -11,7 +11,7 @@ func TestBNode(t *testing.T) {
 
 	const expectedValue = 1
 
-	testname := fmt.Sprintf("BNode() => %v \n", expectedValue)
+	testname := fmt.Sprintf("BNode.() => %v \n", expectedValue)
 	t.Run(testname, func(t *testing.T) {
 
 		ans := BNode{1, nil, nil}
@@ -25,7 +25,7 @@ func TestBNodeValue(t *testing.T) {
 	const expectedValue = 1
 	x := BNode{0, nil, nil}
 
-	testname := fmt.Sprintf("BNode() => %v \n", expectedValue)
+	testname := fmt.Sprintf("BNode.Value() => %v \n", expectedValue)
 	t.Run(testname, func(t *testing.T) {
 
 		x.setValue(1)
@@ -41,7 +41,7 @@ func TestBNodeLeft(t *testing.T) {
 	y := BNode{0, nil, nil}
 	left := BNode{2, nil, nil}
 
-	testname := fmt.Sprintf("BNode() => %v \n", y)
+	testname := fmt.Sprintf("BNode.Left() => %v \n", y)
 	t.Run(testname, func(t *testing.T) {
 
 		y.setLeft(left)
@@ -61,7 +61,7 @@ func TestBNodeRight(t *testing.T) {
 	y := BNode{0, nil, nil}
 	right := BNode{2, nil, nil}
 
-	testname := fmt.Sprintf("BNode() => %v \n", y)
+	testname := fmt.Sprintf("BNode.Right() => %v \n", y)
 	t.Run(testname, func(t *testing.T) {
 
 		y.setRight(right)
@@ -70,6 +70,30 @@ func TestBNodeRight(t *testing.T) {
 		fmt.Printf("y: %v\n", y)
 
 		assert.Equal(t, expectedLeftValue, y.getRight().getValue())
+
+	})
+}
+
+func TestBNodeLeaft(t *testing.T) {
+
+	const expectedLeaft = true
+	const expectedNotLeaft = false
+
+	x := BNode{}
+	y := BNode{}
+	z := BNode{}
+
+	testname := fmt.Sprintf("BNode.Leaft() => %v \n", y)
+	t.Run(testname, func(t *testing.T) {
+
+		z.setLeft(x)
+		z.setRight(y)
+
+		fmt.Printf("PARENT NODE: %v\n", z)
+
+		assert.Equal(t, expectedLeaft, x.isLeaf())
+		assert.Equal(t, expectedLeaft, y.isLeaf())
+		assert.Equal(t, expectedNotLeaft, z.isLeaf())
 
 	})
 }
