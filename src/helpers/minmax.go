2d53a4f7fb54e876f11fe3198e8c20335c8fdd32
Gonzalo Diaz <devel@gon.cl>
2022-07-22 12:07:21 -0400

    helpers: Min and Max of a list (slice) of int values

--- src/helpers/minmax.go
+++ src/helpers/minmax.go
@@ -1,5 +1,9 @@
 package helpers
 
+import "errors"
+
+const __ERR_EMPTY_LIST__ = "list is empty"
+
 func IntMin(a, b int) int {
 	if a < b {
 		return a
@@ -12,4 +16,32 @@ func IntMax(a, b int) int {
 		return a
 	}
 	return b
+}
+
+func IntMinOfMany(list []int) (int, error) {
+	if len(list) <= 0 {
+		return 0, errors.New(__ERR_EMPTY_LIST__)
+	}
+
+	min := list[0]
+
+	for i := 1; i < len(list); i += 1 {
+		min = IntMin(list[i], min)
+	}
+
+	return min, nil
+}
+
+func IntMaxOfMany(list []int) (int, error) {
+	if len(list) <= 0 {
+		return 0, errors.New(__ERR_EMPTY_LIST__)
+	}
+
+	min := list[0]
+
+	for i := 1; i < len(list); i += 1 {
+		min = IntMax(list[i], min)
+	}
+
+	return min, nil
 }
