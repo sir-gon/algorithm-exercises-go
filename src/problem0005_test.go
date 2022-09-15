8652a6476ea3e366131d32cadd77151c6ccd95ae
Gonzalo Diaz <devel@gon.cl>
2022-09-15 10:37:57 -0300

    Problem 0005: New (optimized) solution.

--- src/problem0005_test.go
+++ src/problem0005_test.go
@@ -32,3 +32,31 @@ func TestProblem0005BruteForce(t *testing.T) {
 		assert.Equal(t, expectedSolution, ans)
 	})
 }
+
+func TestProblem0005Basic(t *testing.T) {
+
+	expectedSolution := 2520
+	bottom := 1
+	top := 10
+
+	testname := fmt.Sprintf("Problem0005(%d, %d) => %v \n", bottom, top, expectedSolution)
+	t.Run(testname, func(t *testing.T) {
+
+		ans := Problem0005(bottom, top)
+		assert.Equal(t, expectedSolution, ans)
+	})
+}
+
+func TestProblem0005Full(t *testing.T) {
+
+	expectedSolution := 232792560
+	bottom := 1
+	top := 20
+
+	testname := fmt.Sprintf("Problem0005(%d, %d) => %v \n", bottom, top, expectedSolution)
+	t.Run(testname, func(t *testing.T) {
+
+		ans := Problem0005(bottom, top)
+		assert.Equal(t, expectedSolution, ans)
+	})
+}
