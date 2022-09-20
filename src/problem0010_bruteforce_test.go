858987da974655b18aaa9ca577e088a4aa336d07
Gonzalo Diaz <devel@gon.cl>
2022-09-20 17:55:18 -0300

    [BUGFIX] Problem 0010: build problem fixed.

--- src/problem0010_bruteforce_test.go
+++ src/problem0010_bruteforce_test.go
@@ -20,16 +20,16 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestProblem0010(t *testing.T) {
+func TestProblem0010BruteForce(t *testing.T) {
 
 	const expectedSolution = 142913828922
 	const bottom = 1
 	const top = 2000000
 
-	testname := fmt.Sprintf("Problem0010() => %v \n", expectedSolution)
+	testname := fmt.Sprintf("Problem0010(%d, %d) => %v \n", bottom, top, expectedSolution)
 	t.Run(testname, func(t *testing.T) {
 
-		ans := Problem0010()
+		ans := Problem0010(bottom, top)
 		assert.Equal(t, expectedSolution, ans)
 	})
 }
