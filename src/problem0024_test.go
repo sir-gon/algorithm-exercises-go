46437a27ba0c6cb77dfcb7ab8da01c9f614b0a99
Gonzalo Diaz <devel@gon.cl>
2023-06-27 20:39:58 -0400

    Problem 0024: solved.

---
+++ src/problem0024_test.go
@@ -0,0 +1,52 @@
+/**
+ * Lexicographic Permutations
+ *
+ * https://projecteuler.net/problem=24
+ *
+ * A permutation is an ordered arrangement of objects.
+ * For example, 3124 is one possible permutation of the digits 1, 2, 3 and 4.
+ * If all of the permutations are listed numerically or alphabetically,
+ * we call it lexicographic order. The lexicographic permutations of 0, 1 and 2 are:
+ *
+ * 012   021   102   120   201   210
+ *
+ * What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
+ */
+
+package projecteuler
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProblem0024SmallCase(t *testing.T) {
+
+	expectedSolution := []string{"A", "D", "C", "B"}
+	inputElements := strings.Split("ABCD", "")
+	const inputPermutationToFind = 6
+
+	testname := fmt.Sprintf("Problem0024(%+v, %d) => %v \n", inputElements, inputPermutationToFind, expectedSolution)
+	t.Run(testname, func(t *testing.T) {
+
+		ans := Problem0024(inputElements, inputPermutationToFind)
+		assert.Equal(t, expectedSolution, ans)
+	})
+}
+
+func TestProblem0024(t *testing.T) {
+
+	expectedSolution := []string{"2", "7", "8", "3", "9", "1", "5", "4", "6", "0"}
+	inputElements := strings.Split("0123456789", "")
+	const inputPermutationToFind = 1000000
+
+	testname := fmt.Sprintf("Problem0024(%+v, %d) => %v \n", inputElements, inputPermutationToFind, expectedSolution)
+	t.Run(testname, func(t *testing.T) {
+
+		ans := Problem0024(inputElements, inputPermutationToFind)
+		assert.Equal(t, expectedSolution, ans)
+	})
+}
