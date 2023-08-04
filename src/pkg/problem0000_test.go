0c73005dffc36632d330288798a9cff36a1bc6bf
Gonzalo Diaz <devel@gon.cl>
2023-08-04 15:40:01 -0400

    [REFACTOR] code reorganization

---
+++ src/pkg/problem0000_test.go
@@ -0,0 +1,28 @@
+/**
+ *
+ * https://projecteuler.net/problem=XX
+ *
+ *
+ */
+
+package exercises
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestProblem0000(t *testing.T) {
+
+	expectedSolution := 0
+	input := 0
+
+	testname := fmt.Sprintf("Problem0000(%d) => %v \n", input, expectedSolution)
+	t.Run(testname, func(t *testing.T) {
+
+		ans := Problem0000()
+		assert.Equal(t, expectedSolution, ans)
+	})
+}
