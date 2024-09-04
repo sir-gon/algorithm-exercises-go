979f0a127066084b44a2bf390223d7b6e4354c1a
Gonzalo Diaz <devel@gon.cl>
2024-09-03 21:08:18 -0400

    [Hacker Rank]: Go (Basic) certification problem ✓. Load test case data from JSON.

--- exercises/hackerrank/certification/goremaindersorting_test.go
+++ exercises/hackerrank/certification/goremaindersorting_test.go
@@ -2,34 +2,43 @@ package hackerrank
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"gon.cl/algorithms/utils"
 )
+
+type TestCase struct {
+	Input    []string `json:"input"`
+	Expected []string `json:"expected"`
+}
+
+var testCases []TestCase
+
+// You can use testing.T, if you want to test the code without benchmarking
+func setupSuite(t testing.TB) {
+	wd, _ := os.Getwd()
+	filepath := wd + "/goremaindersorting.testcases.json"
+	t.Log("Setup test cases from JSON: ", filepath)
+
+	var _, err = utils.LoadJSON(filepath, &testCases)
+	if err != nil {
+		t.Log(err)
+	}
+}
 
 func TestSolveMeFirst(t *testing.T) {
 
-	a := []string{
-		"Colorado",
-		"Utah",
-		"Montana",
-		"Wisconsin",
-		"Oregon",
-		"Maine",
-	}
-	expectedSolution := []string{
-		"Oregon",
-		"Wisconsin",
-		"Montana",
-		"Utah",
-		"Colorado",
-		"Maine",
-	}
+	setupSuite(t)
 
-	testname := fmt.Sprintf("RemainderSorting(%v) => %v \n", a, expectedSolution)
-	t.Run(testname, func(t *testing.T) {
+	for _, tt := range testCases {
+		testname := fmt.Sprintf("RemainderSorting(%v) => %v \n", tt.Input, tt.Expected)
+		t.Run(testname, func(t *testing.T) {
 
-		ans := RemainderSorting(a)
-		assert.Equal(t, expectedSolution, ans)
-	})
+			ans := RemainderSorting(tt.Input)
+			assert.Equal(t, tt.Expected, ans)
+		})
+
+	}
 }
