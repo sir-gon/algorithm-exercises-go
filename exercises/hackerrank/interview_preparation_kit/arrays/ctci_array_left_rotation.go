e93b89c5e0acf7c1f5768b35449d5ddf7780dc89
Gonzalo Diaz <devel@gon.cl>
2025-01-28 12:52:49 -0300

    [Hacker Rank] Interview Preparation Kit: Arrays: Left Rotation. WIP.

---
+++ exercises/hackerrank/interview_preparation_kit/arrays/ctci_array_left_rotation.go
@@ -0,0 +1,10 @@
+package hackerrank
+
+func rotLeft(a []int32, d int32) []int32 {
+	// Write your code here
+	return []int32{}
+}
+
+func RotLeft(a []int32, d int32) []int32 {
+	return rotLeft(a, d)
+}
