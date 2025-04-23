7bf49d4d70ecba9fdb086c6c1d0f3216505ae2ff
Gonzalo Diaz <devel@gon.cl>
2025-04-22 20:50:21 -0400

    [Hacker Rank] Interview Preparation Kit: Dictionaries and Hashmaps: Two Strings. Solved ✓.

---
+++ exercises/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/two_strings.go
@@ -0,0 +1,32 @@
+/**
+ * @link Problem definition [[docs/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/two_strings.md]]
+ */
+
+package hackerrank
+
+const __TWO_STRINGS_YES__ = "Yes"
+const __TWO_STRINGS_NO__ = "No"
+
+func twoStringsCompute(s1 string, s2 string) bool {
+	for _, letter := range s1 {
+		for _, n := range s2 {
+			if letter == n {
+				return true
+			}
+		}
+	}
+
+	return false
+}
+
+func twoStrings(s1 string, s2 string) string {
+	if twoStringsCompute(s1, s2) {
+		return __TWO_STRINGS_YES__
+	}
+
+	return __TWO_STRINGS_NO__
+}
+
+func TwoStrings(s1 string, s2 string) string {
+	return twoStrings(s1, s2)
+}
