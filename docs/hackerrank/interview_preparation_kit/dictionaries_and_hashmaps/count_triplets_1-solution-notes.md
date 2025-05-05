814d32b8e7f0c71052b410713c1797c7ff36334a
Gonzalo Diaz <devel@gon.cl>
2025-05-05 11:31:10 -0400

    [Hacker Rank] Interview Preparation Kit: Dictionaries and Hashmaps: Count Triplets. Solved ✅.

---
+++ docs/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/count_triplets_1-solution-notes.md
@@ -0,0 +1,13 @@
+# [Dictionaries and Hashmaps: Count Triplets](https://www.hackerrank.com/challenges/count-triplets-1)
+
+- Difficulty:  `#medium`
+- Category: `#ProblemSolvingIntermediate`
+
+## Working solution
+
+This solution in O(N), is based on considering that each
+number analyzed is in the center of the triplet and asks
+"how many" possible cases there are on the left and
+right to calculate how many possible triplets are formed.
+
+- Source: [Hackerrank - Count Triplets Solution](https://www.thepoorcoder.com/hackerrank-count-triplets-solution/)
---
+++ exercises/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/count-triplets-1.go
@@ -0,0 +1,55 @@
+/**
+ * @link Problem definition [[docs/hackerrank/interview_preparation_kit/dictionaries_and_hashmaps/count_triplets_1.md]]
+ */
+
+package hackerrank
+
+func countTripletsBruteForce(arr []int64, r int64) int64 {
+	size := len(arr)
+	counter := int64(0)
+	for i := 0; i < size-2; i++ {
+		for j := i + 1; j < size-1; j++ {
+			for k := j + 1; k < size; k++ {
+				if r*arr[i] == arr[j] && r*arr[j] == arr[k] {
+					counter++
+				}
+			}
+		}
+	}
+	return counter
+}
+
+func countTriplets(arr []int64, r int64) int64 {
+	aCounter := make(map[int64]int64)
+	bCounter := make(map[int64]int64)
+	triplets := int64(0)
+
+	for _, item := range arr {
+		if _, ok := aCounter[item]; ok && aCounter[item] > 0 {
+			aCounter[item] += 1
+		} else {
+			aCounter[item] = 1
+		}
+	}
+
+	for _, item := range arr {
+		j := item / r
+		k := item * r
+
+		aCounter[item]--
+		if bCounter[j] > 0 && aCounter[k] > 0 && item%r == 0 {
+			triplets += bCounter[j] * aCounter[k]
+		}
+		bCounter[item]++
+	}
+
+	return triplets
+}
+
+func CountTriplets(arr []int64, r int64) int64 {
+	return countTriplets(arr, r)
+}
+
+func CountTripletsBruteForce(arr []int64, r int64) int64 {
+	return countTripletsBruteForce(arr, r)
+}
