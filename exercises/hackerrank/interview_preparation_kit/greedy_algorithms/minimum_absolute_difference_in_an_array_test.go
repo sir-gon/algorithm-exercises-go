57361ffa7bd546e45428bc8b1cbb67004c06ebf0
Gonzalo Diaz <devel@gon.cl>
2025-05-09 00:42:20 -0400

    [Hacker Rank] Interview Preparation Kit: Greedy Algorithms: Minimum Absolute Difference in an Array. New big test case added.

--- exercises/hackerrank/interview_preparation_kit/greedy_algorithms/minimum_absolute_difference_in_an_array_test.go
+++ exercises/hackerrank/interview_preparation_kit/greedy_algorithms/minimum_absolute_difference_in_an_array_test.go
@@ -15,6 +15,7 @@ type MinimumAbsoluteDifferenceInAnArrayTestCase struct {
 }
 
 var MinimumAbsoluteDifferenceInAnArrayTestCases []MinimumAbsoluteDifferenceInAnArrayTestCase
+var MinimumAbsoluteDifferenceInAnArrayTestCase2 []MinimumAbsoluteDifferenceInAnArrayTestCase
 
 // You can use testing.T, if you want to test the code without benchmarking
 func MinimumAbsoluteDifferenceInAnArraySetupSuite(t testing.TB) {
@@ -28,11 +29,35 @@ func MinimumAbsoluteDifferenceInAnArraySetupSuite(t testing.TB) {
 	}
 }
 
+func MinimumAbsoluteDifferenceInAnArraySetupSuiteTestCase2(t testing.TB) {
+	wd, _ := os.Getwd()
+	filepath := wd + "/minimum_absolute_difference_in_an_array.testcases.json"
+	t.Log("Setup test cases from JSON: ", filepath)
+
+	var _, err = utils.LoadJSON(filepath, &MinimumAbsoluteDifferenceInAnArrayTestCase2)
+	if err != nil {
+		t.Log(err)
+	}
+}
+
 func TestMinimumAbsoluteDifferenceInAnArray(t *testing.T) {
 
 	MinimumAbsoluteDifferenceInAnArraySetupSuite(t)
 
 	for _, tt := range MinimumAbsoluteDifferenceInAnArrayTestCases {
+		testname := fmt.Sprintf("MinimumAbsoluteDifferenceInAnArray(%v) => %v \n", tt.Input, tt.Expected)
+		t.Run(testname, func(t *testing.T) {
+			ans := MinimumAbsoluteDifferenceInAnArray(tt.Input)
+			assert.Equal(t, tt.Expected, ans)
+		})
+	}
+}
+
+func TestMinimumAbsoluteDifferenceInAnArrayBigCase(t *testing.T) {
+
+	MinimumAbsoluteDifferenceInAnArraySetupSuiteTestCase2(t)
+
+	for _, tt := range MinimumAbsoluteDifferenceInAnArrayTestCase2 {
 		testname := fmt.Sprintf("MinimumAbsoluteDifferenceInAnArray(%v) => %v \n", tt.Input, tt.Expected)
 		t.Run(testname, func(t *testing.T) {
 			ans := MinimumAbsoluteDifferenceInAnArray(tt.Input)
